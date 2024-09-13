703a23041be2c9919c40b539c0cb958524055f5e
Maya Demurovi <maya.demurovi@gmail.com>
2024-09-14 00:17:39 +0400

    [577] [Time Beats: 98.77%] - LeetPush

---
+++ 577-Employee-Bonus.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT Employee.name, Bonus.bonus FROM Employee
+LEFT JOIN Bonus on Employee.empId = Bonus.empId 
+WHERE Bonus.bonus < 1000 or Bonus.bonus IS NULL 
